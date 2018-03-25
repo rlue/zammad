c7af42f9beacf08b86e4e16cf7cc3408df978388
Ryan Lue <hello@ryanlue.com>
2018-03-25 08:36:23 +0800

    Troubleshoot CI environment (variable: trigger-docker-build.sh top-level comments)

--- contrib/travis-ci.org/trigger-docker-build.sh
+++ contrib/travis-ci.org/trigger-docker-build.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-#
-# trigger build of https://github.com/zammad/zammad-docker on https://travis-ci.org/zammad/zammad-docker and upload it to https://hub.docker.com/r/zammad/zammad
-#
 
 touch foo
